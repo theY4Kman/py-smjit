0097b8a3557e28de4bdb73205462c719828a955b
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2011-11-28 02:33:45 -0800

    Changed module name to smjit

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from Cython.Distutils import build_ext
 SOURCEMOD_PATH = '/Users/they4kman/programming/sourcemod-central'
 
 ext = Extension(
-    "cpp_smjit",                 # name of extension
-    [ "cpp_smjit.pyx", 'cpp_smjit_iface.cpp' ],     # filename of our Cython source
+    "smjit",                 # name of extension
+    [ 'smjit.pyx', 'cpp_smjit_iface.cpp' ],     # filename of our Cython source
     language="c++",              # this causes Cython to create C++ source
     include_dirs=[SOURCEMOD_PATH + '/public', SOURCEMOD_PATH + '/public/sourcepawn', SOURCEMOD_PATH + '/sourcepawn/jit' ],          # usual stuff
     libraries=[  ],             # ditto
